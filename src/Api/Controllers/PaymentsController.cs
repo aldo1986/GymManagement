841b04362d9cd649a7546755525b481c76ba07f1
aldo1986 <aldo.fuentes@gmail.com>
2025-07-26 17:40:23 -0600

    las change

--- src/Api/Controllers/PaymentsController.cs
+++ src/Api/Controllers/PaymentsController.cs
@@ -2,9 +2,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Application.Payments.Commands;
 using Application.Application.Payments.Commands;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Api.Controllers;
-
+[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class PaymentsController : ControllerBase
